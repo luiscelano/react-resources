d85fb594e9165b73cef5be5cbd12ba4fd7664638
luiscelano <luiscelano123@live.com>
2023-09-07 23:28:05 -0600

    refactor: changed imports

---
+++ react-app-example/src/example4.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MyComponent() {
+  return <span>Hello World!</span>;
+}
+
+export default MyComponent;
--- react-app-example/src/index.js
+++ react-app-example/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import Ejemplo1 from './example1';
 import Ejemplo2 from './example2';
 import Ejemplo3 from './example3';
+import Ejemplo4 from './example4';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Ejemplo1 /> */}
     {/* <Ejemplo2 /> */}
-    <Ejemplo3 />
+    {/* <Ejemplo3 /> */}
+    <Ejemplo4 />
   </React.StrictMode>
 );
